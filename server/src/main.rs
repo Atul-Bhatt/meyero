34a77d93a677cfea92c983d062af19a23efcbc43
Atul Bhatt <atulbhatt61@gmail.com>
2024-02-12 22:50:29 +0530

    server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, HttpRequest, HttpResponse, Error, get, web, Responder, Result};
+use actix_web::middleware::Logger;
+use actix_cors::Cors;
+use actix_rt;
+
+use env_logger::Env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| {
+        let cors = Cors::permissive();
+
+        App::new()
+            .wrap(cors)
+            .wrap(Logger::default())
+            .service(get_julia_image)
+            .service(web::resource("/ws").route(web::get().to(julia_ws)))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
