d7c36aee6ca70b3fc3f97054e6479e14c167ee13
Atul Bhatt <atulbhatt61@gmail.com>
2024-02-14 12:40:11 +0530

    react app is generating images

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import JuliaCanvas from "./components/juliacanvas";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-body">
+      <JuliaCanvas
+        api="get"
+        width={800}
+        height={500}
+        url="localhost"
+        port="8080"
+        className="App-canvas">
+      </JuliaCanvas>
     </div>
   );
 }
