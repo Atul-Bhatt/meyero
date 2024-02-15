9015177b4080b18aa73178e2a6faabe74b5f5578
Atul Bhatt <atulbhatt61@gmail.com>
2024-02-15 22:20:37 +0530

    publish handler

--- server/src/handler.rs
+++ server/src/handler.rs
@@ -1,18 +1,2 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Deserialize, Serialize)]
-pub struct RegisterRequest {
-    user_id: usize,
-}
-
-#[derive(Deserialize, Serialize)]
-pub struct RegisterResponse {
-    url: String,
-}
-
-#[derive(Deserialize, Serialize)]
-pub struct Event {
-    topic: String,
-    user_id: Option<usize>,
-    message: String,
-}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,19 @@
-use actix_web::{App, HttpServer, HttpRequest, HttpResponse, Error, get, web, Responder, Result};
+use std::collections::HashMap;
+
+use actix_web::{App, HttpServer, HttpRequest, HttpResponse, Error, get, web, Responder, Result, post};
 use actix_web::middleware::Logger;
 use actix_cors::Cors;
 use actix_rt;
 use env_logger::Env;
 use serde::{Serialize, Deserialize};
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, RwLock};
 use actix_ws::Message;
+use std::sync::Arc;
 
 mod handler;
 mod websocket;
+
+type Clients = Arc<RwLock<HashMap<String, Client>>>;
 
 #[derive(Serialize)]
 struct ApiData {
@@ -28,6 +33,23 @@ pub struct Client {
     pub sender: Option<mpsc::UnboundedSender<std::result::Result<Message, Error>>>,
 }
 
+#[derive(Deserialize, Serialize)]
+pub struct RegisterRequest {
+    user_id: usize,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct RegisterResponse {
+    url: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Event {
+    topic: String,
+    user_id: Option<usize>,
+    message: String,
+}
+
 #[get("/health")]
 async fn health() -> Result<impl Responder> {
     // let obj = ApiData {
@@ -36,6 +58,26 @@ async fn health() -> Result<impl Responder> {
     // };
     // Ok(web::Json(obj))
     Ok("Meyero is live...")
+}
+
+#[post("publish")]
+async fn publish_handler(body: Event, clients: Clients) -> Result<impl Reply> {
+    clients
+        .read()
+        .await
+        .iter()
+        .filter(|(_, client)| match body.user_id {
+            Some(v) => client.user_id == v,
+            None => true,
+        })
+        .filter(|(_, client)| client.topics.contains(&body.topic))
+        .for_each(|(_, client)| {
+            if let Some(sender) = &client.sender {
+                let _ = sender.send(Ok(Message::Text(body.message.clone().into())));
+            }
+        });
+
+    Ok(HttpResponse::Accepted())
 }
 
 #[actix_web::main]
