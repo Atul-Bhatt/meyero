e281e7d1b72d39060ab2488edc432f2512fb1280
Atul Bhatt <atulbhatt61@gmail.com>
2024-09-03 16:43:22 +0530

    removed old code, new server

--- client/src/node.rs
+++
@@ -1,5 +0,0 @@
-use mainline::Dht;
-
-pub fn dht_client() -> Dht {
-    return Dht::server().unwrap()
-}
--- client/src/seed.rs
+++
@@ -1,4 +0,0 @@
-
-pub fn get_first_seed_server() -> String {
-    "http://localhost:4000".to_string()
-}
