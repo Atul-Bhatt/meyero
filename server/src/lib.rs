3be50a239a9a314156b05e9c4e7d062d8ef94d99
Atul Bhatt <atulbhatt61@gmail.com>
2024-09-04 17:24:55 +0530

    postgres connection

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,6 +1,7 @@
 use actix_web::{get, App, HttpServer, Responder, HttpRequest, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use sqlx::PgPool;
 
 #[get("/health_check")]
 async fn health_check(_req: HttpRequest) -> impl Responder {
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,12 @@
 use server::run;
 use std::net::TcpListener;
+use sqlx::PgPool;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let pool = PgPool::connect(&std::env::var("DATABASE_URL").unwrap()).await.unwrap();
+    sqlx::migrate!().run(&pool).await.unwrap();
+
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind port");
     println!("Running on port: {}", listener.local_addr().unwrap().port());
