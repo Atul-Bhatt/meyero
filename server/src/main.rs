7b29446fb828e48ccdbdbf2627581be766ce94a2
Atul Bhatt <atulbhatt61@gmail.com>
2024-11-24 20:53:48 +0530

    url moved to env

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,10 +2,14 @@ mod websocket;
 
 use tokio_tungstenite::accept_async;
 use tokio::net::TcpListener;
+use std::env;
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    dotenv().ok();
+    let url = env::var("WEBSOCKET_URL").expect("Error getting _WEBSOCKET_URL");
+    let listener = TcpListener::bind(url).await.unwrap();
 
     while let Ok((stream, _)) = listener.accept().await {
         let ws_stream = accept_async(stream).await.unwrap();
