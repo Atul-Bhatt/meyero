841487de7771b16134bef74e517fedf6da2c1adc
Atul Bhatt <atulbhatt61@gmail.com>
2024-12-24 18:05:26 +0530

    jwt token migration

---
+++ server/migrations/20241224123021_jwt_token.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+CREATE TABLE if not exists token ( 
+	id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+	user_id UUID NOT NULL REFERENCES users,
+	token TEXT NOT NULL,
+	expires_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW() + INTERVAL '1 hours',
+	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	revoked TIMESTAMP
+);
