ae7558b69574f5f51f1a81aef15d28908a060dc9
Atul Bhatt <atulbhatt61@gmail.com>
2025-01-24 15:49:51 +0530

    added message migration

---
+++ server/migrations/20250124101611_create_message_table.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+CREATE TABLE if not exists message (
+    id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+    from_user UUID NOT NULL REFERENCES users,
+    to_user UUID NOT NULL REFERENCES users,
+    message TEXT,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
