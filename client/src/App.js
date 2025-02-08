33b1eedb52437ecf8b5efff4b8449a81783d60c6
Atul Bhatt <atulbhatt61@gmail.com>
2025-02-08 10:43:29 +0530

    frontend routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import WebSocket from './WebSocket/WebSocket'
+import Home from './Pages/Home/Home'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <LoginSignup /> 
-        {/* <WebSocket/> */}
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route path="/websocket" element={<WebSocket />} />
+      </Routes>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <LoginSignup /> 
+    //     {/* <WebSocket/> */}
+    //   </header>
+    // </div>
   );
 }
 
---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import './Home.css';
+
+const Home = () => {
+	return (
+        <h1>This is the Home page</h1>
+	);
+};
+
+export default Home;
