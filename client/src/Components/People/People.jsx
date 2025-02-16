675301aaa1d8ee5e433dca1dbbfa8a9c1926c5b4
Atul Bhatt <atulbhatt61@gmail.com>
2025-02-16 13:26:57 +0530

    avatar in user list

--- client/src/Components/People/People.jsx
+++ client/src/Components/People/People.jsx
@@ -1,8 +1,10 @@
 import './People.css';
+import { Avatar } from '@material-ui/core';
 
-const People = ({name}) => {
+const People = ({name, photoUrl}) => {
     return (
-        <div>
+        <div className="people__main">
+            <Avatar src={photoUrl}/>
             <h3>{name}</h3>
         </div>
     );
