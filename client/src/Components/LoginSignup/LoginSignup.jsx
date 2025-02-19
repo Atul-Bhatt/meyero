27a281a78567fb4003f4a021f33131b5a19839e0
Atul Bhatt <atulbhatt61@gmail.com>
2025-02-19 15:13:01 +0530

    change login page

--- client/src/Components/LoginSignup/LoginSignup.jsx
+++ client/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,122 +1,74 @@
 import './LoginSignup.css'
 
-import google_logo from '../Assets/google_logo.png'
-import apple_logo from '../Assets/apple_logo.png'
-import login_background from '../Assets/login_background.jpg'
-import axios from 'axios'
-import React from 'react'
-import { Link } from 'react-router-dom'
-class LoginSignup extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: ""
-        };
-    }
+import google_logo from '../Assets/google_logo.png';
+import apple_logo from '../Assets/apple_logo.png';
+import login_background from '../Assets/login_background.jpg';
+import axios from 'axios';
+import { useState } from 'react';
 
-    handleOnChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+const LoginSignup = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const url = 'http://localhost:8081/user/login';
 
-    handleSubmit = e => {
-        e.preventDefault();
-        // axios.post("http://localhost:8081/user/login", {
-        //     id: "9eb564d2-82a5-40f3-8fd8-9a0f52676c49", // Not actually used
-        //     username: this.state.username,
-        //     password: this.state.password
-        // })
-        // .then(reponse => {
-        //     console.log(reponse);
-        //     alert('login success')
-        //     navigate('/')
-        // })
-        // .catch(error => {
-        //     console.log(error);
-        //     alert('login failed')
-        // });
-
-        // this.setState({
-        //     username: "",
-        //     password: ""
-        // });
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
         try {
-            const response = axios.post(
-                "http://localhost:8081/user/login",
-                JSON.stringify(this.state),
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "*"
-                    },
-                    withCredentials: true,
-                }
-            );
-            console.log(response?.data?.token);
-
-            this.setState ({
-                username: "",
-                password: ""
+            const response = axios.post(url, {
+                username,
+                password
             });
-        } catch (err) {
-            console.log("Login Failed")
+
+            console.log(response.data);
+        } catch(error) {
+            console.log(error);
         }
     };
 
-    navigateToHome = e => {
-        window.location("http://localhost:3000/")
-    }
-
-    render() {
-        return (
-            <div className="main-container">
-            <div className="left-container">
-                <img src={login_background} alt="" className="background-image" />
+    return (
+        <div className="main-container">
+        <div className="left-container">
+            <img src={login_background} alt="" className="background-image" />
+        </div>
+        <div className='right-container'>
+        <div className="header">
+            <div className="text">Log in</div>
+        </div>
+        <div className="other-logins">
+            <button className="other-login-button">
+                <img src={google_logo} alt="google_logo"></img>
+                <span>Continue with Google</span>
+            </button>
+            <br/>
+            <button className="other-login-button">
+                <img src={apple_logo} alt="apple_logo" />
+                <span>Continue with Apple</span>
+            </button>
+        </div>
+        <div className="line-container">
+            <p><span>OR</span></p>
+        </div> 
+        <div className="inputs">
+            <form onSubmit={handleSubmit}>
+            <div className="input">
+                <input type="text"
+                    placeholder="Username"
+                    onChange={(event) => setUsername(event.target.value)}/>
             </div>
-            <div className='right-container'>
-            <div className="header">
-                <div className="text">Log in</div>
+            <div className="input">
+                <input type="password"
+                    placeholder="Password"
+                    onChange={(event) => setPassword(event.target.value)}/>
             </div>
-            <div className="other-logins">
-                <button className="other-login-button">
-                    <img src={google_logo} alt="google_logo"></img>
-                    <span>Continue with Google</span>
-                </button>
-                <br/>
-                <button className="other-login-button">
-                    <img src={apple_logo} alt="apple_logo" />
-                    <span>Continue with Apple</span>
-                </button>
+            <div className="submit-container">
+                <button type="submit">LOG IN</button>
             </div>
-            <div className="line-container">
-                <p><span>OR</span></p>
-            </div> 
-            <div className="inputs">
-                <form onSubmit={this.navigteToHome}>
-                <div className="input">
-                    <input type="text"
-                        placeholder="Username"
-                        //value={this.state.username}
-                        onChange={this.handleOnChange}/>
-                </div>
-                <div className="input">
-                    <input type="password"
-                        placeholder="Password"
-                        //value={this.state.password}
-                        onChange={this.handleOnChange}/>
-                </div>
-                <div className="submit-container">
-                    <button type="submit">LOG IN</button>
-                </div>
-                </form>
-            </div>
-            </div>
-            </div>
-        );
-    };
+            </form>
+        </div>
+        </div>
+        </div>
+    );
 }
 
 export default LoginSignup;
