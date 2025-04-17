8fd80ecc89cf997459e9313cc6ea900a9f0f463e
Atul Bhatt <atulbhatt61@gmail.com>
2025-04-17 17:27:14 +0530

    delete token on logout

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,11 @@ import './Navbar.css';
 import Avatar from '@mui/material/Avatar'
 
 const Navbar = ({photoUrl, userName}) => {
+    const handleLogOut = () => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("userId")
+        localStorage.removeItem("userName")
+    }
 	return (
     <nav className="navbar">
       <a href="/" className="navbar-logo">Meyero</a>
@@ -10,11 +15,10 @@ const Navbar = ({photoUrl, userName}) => {
 
       <div className="navbar-user">
         <Avatar src={photoUrl} />
-        <div className="navbar-name">John Doe</div>
+        <div className="navbar-name">{localStorage.getItem("userName")}</div>
         <div className="navbar-dropdown">
           <a href="/profile">Profile</a>
-          <a href="/settings">Settings</a>
-          <a href="/logout">Logout</a>
+          <a href="/login" onClick={handleLogOut}>Logout</a>
         </div>
       </div>
     </nav>
